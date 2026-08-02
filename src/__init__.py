d68a4c8ac54651e8650439560b3c6e052d8ee59c
Peter Pallaoro <>
2026-08-02 07:46:25 +0000

    Trying to solve package issues.

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,10 @@ from .BaseAdapter import BaseAdapter
 from .hpo.HPOAdapter  import HPOAdapter
 from .sct.SCTAdapter  import SCTAdapter
 from .umls.UMLSAdapter import UMLSAdapter
+
+__all__ = [
+    "BaseAdapter",
+    "HPOAdapter",
+    "SCTAdapter",
+    "UMLSAdapter",
+]
