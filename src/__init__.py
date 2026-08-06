9078e6e171f0ec600bc731faad033b19c0d0fd67
Peter Pallaoro <>
2026-08-06 10:13:25 +0000

    Added Utils to the library.

--- src/__init__.py
+++ src/__init__.py
@@ -3,13 +3,15 @@ import sys
 # Prevent Python from generating .pyc files (compiled bytecode files)
 sys.dont_write_bytecode = True
 
-from .BaseAdapter import BaseAdapter
-from .hpo.HPOAdapter  import HPOAdapter
-from .sct.SCTAdapter  import SCTAdapter
-from .umls.UMLSAdapter import UMLSAdapter
+from .BaseAdapter           import BaseAdapter
+from .BaseAdapterUtils      import BaseAdapterUtils
+from .hpo.HPOAdapter        import HPOAdapter
+from .sct.SCTAdapter        import SCTAdapter
+from .umls.UMLSAdapter      import UMLSAdapter
 
 __all__ = [
     "BaseAdapter",
+    "BaseAdapterUtils",
     "HPOAdapter",
     "SCTAdapter",
     "UMLSAdapter",
