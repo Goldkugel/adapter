4c1105d422d5551a77acd5c75e864a644996126e
Peter Pallaoro <>
2026-08-12 12:18:50 +0000

    Added all variables to init file.

--- src/__init__.py
+++ src/__init__.py
@@ -6,8 +6,11 @@ sys.dont_write_bytecode = True
 from .BaseAdapter           import BaseAdapter
 from .BaseAdapterUtils      import writeHugeCSV, writeCSV, labelClass, definitionClass, commentClass, referenceClass, childrenClass, synonymClass
 from .hpo.HPOAdapter        import HPOAdapter
+from .hpo.HPOAdapterUtils   import semanticClass, exactSynonymClass, relatedSynonymClass, broadSynonymClass, narrowSynonymClass, sourceType, expertSynonymType, laypersonSynonymType, abbreviationSynonymType, obsoleteSynonymType, pluralFormSynonymType, ukSpellingSynonymType, allelicRequirementSynonymType
 from .sct.SCTAdapter        import SCTAdapter
+from .sct.SCTAdapterUtils   import referenceOntology
 from .umls.UMLSAdapter      import UMLSAdapter
+from .umls.UMLSAdapterUtils import sourceAbbreviation
 
 __all__ = [
     "BaseAdapter",
@@ -21,5 +24,21 @@ __all__ = [
     "commentClass",
     "referenceClass", 
     "childrenClass", 
-    "synonymClass"
+    "synonymClass",
+    "semanticClass", 
+    "exactSynonymClass", 
+    "relatedSynonymClass", 
+    "broadSynonymClass", 
+    "narrowSynonymClass", 
+    "sourceType", 
+    "expertSynonymType", 
+    "laypersonSynonymType", 
+    "abbreviationSynonymType", 
+    "obsoleteSynonymType", 
+    "pluralFormSynonymType", 
+    "ukSpellingSynonymType", 
+    "allelicRequirementSynonymType",
+    "sourceAbbreviation",
+    "referenceOntology"
 ]
+
